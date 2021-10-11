a5c3e14fef09ed5f6dbae36ed6ba98eff65c21f1
l33th <l33th@protonmail.ch>
2021-10-11 06:50:34 +0200

    Added Add Issue Button

--- src/components/AddIssue/AddIssue.js
+++ src/components/AddIssue/AddIssue.js
@@ -4,25 +4,35 @@ const AddIssue = () => {
 	const [addIssue, setAddIssue] = useState(true);
 	return (
 		<>
-			<div>
-				<h1>Add Issue</h1>
+			<h1>Add Issue</h1>
+			<form action=''>
 				<label>
-					Description <input type='text' />{" "}
+					Description: <input type='text' />{" "}
 				</label>
-                <label>
-                
-				<select name='wmDev' id='wmDev'>
-					<option value='Richard'>Richard</option>
-					<option value='Gilfoyle'>Gilfoyle</option>
-					<option value='Dinesh'>Dinesh</option>
-					<option value='BigHead'>BigHead</option>
-					<option value='Erlick'>Erlick</option>
-					<option value='Jian'>Jian</option>
-					<option value='Jared'>Jared</option>
-					<option value='Morena'>Morena</option>
-				</select>
-                </label>
-			</div>
+				<label>
+					Assign To:{" "}
+					<select name='wmDev' id='wmDev'>
+						<option value='Richard'>Richard</option>
+						<option value='Gilfoyle'>Gilfoyle</option>
+						<option value='Dinesh'>Dinesh</option>
+						<option value='BigHead'>BigHead</option>
+						<option value='Erlick'>Erlick</option>
+						<option value='Jian'>Jian</option>
+						<option value='Jared'>Jared</option>
+						<option value='Morena'>Morena</option>
+					</select>{" "}
+				</label>
+				<label>
+					Priority Level:{" "}
+					<select name='priority' id='priority'>
+						<option value='Low'>Low</option>
+						<option value='Medium'>Medium</option>
+						<option value='High'>High</option>
+						<option value='Critical'>Critical</option>
+					</select>
+				</label>
+				<button type='submit'>Add Issue</button>
+			</form>
 		</>
 	);
 };
