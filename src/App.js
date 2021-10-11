9496e32030f3fa714f245e4ddc776ffc8e53b878
l33th <l33th@protonmail.ch>
2021-10-11 06:44:27 +0200

    Added Labels

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
-import AddIssue from "./components/AddIssue/AddIssue.js";
-import CurrentIssue from "./components/CurrentIssue/CurrentIssue.js";
+import AddIssue from "./components/AddIssue/AddIssue";
+import CurrentIssue from "./components/CurrentIssue/CurrentIssue";
 function App() {
-  const [addIssue, setAddIssue] = useState(true);
-  return (
-    <div className="App">
-      <h1>Bug Tracker</h1>
-      {addIssue ? <AddIssue /> : <CurrentIssue />}
-      <div>
-        <button>Current Issues</button>
-        <button>Add Issue</button>
-      </div>
-    </div>
-  );
+	const [addIssue, setAddIssue] = useState(true);
+	return (
+		<div className='App'>
+			<h1>Bug Tracker</h1>
+			{addIssue ? <AddIssue /> : <CurrentIssue />}
+			<div>
+				<button>Current Issues</button>
+				<button>Add Issue</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/AddIssue/AddIssue.js
+++ src/components/AddIssue/AddIssue.js
@@ -3,9 +3,27 @@ import React, { useState } from "react";
 const AddIssue = () => {
 	const [addIssue, setAddIssue] = useState(true);
 	return (
-		<div>
-			<h1>Add Issue</h1>
-		</div>
+		<>
+			<div>
+				<h1>Add Issue</h1>
+				<label>
+					Description <input type='text' />{" "}
+				</label>
+                <label>
+                
+				<select name='wmDev' id='wmDev'>
+					<option value='Richard'>Richard</option>
+					<option value='Gilfoyle'>Gilfoyle</option>
+					<option value='Dinesh'>Dinesh</option>
+					<option value='BigHead'>BigHead</option>
+					<option value='Erlick'>Erlick</option>
+					<option value='Jian'>Jian</option>
+					<option value='Jared'>Jared</option>
+					<option value='Morena'>Morena</option>
+				</select>
+                </label>
+			</div>
+		</>
 	);
 };
 
