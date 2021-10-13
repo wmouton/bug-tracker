48f53ebee01bb4f458d507d46b428c80e46d8db2
l33th <l33th@protonmail.ch>
2021-10-13 06:55:33 +0200

    Current Issue

--- src/components/CurrentIssue/CurrentIssue.js
+++ src/components/CurrentIssue/CurrentIssue.js
@@ -3,7 +3,15 @@ import React, { useState } from "react";
 const CurrentIssue = () => {
 	return (
 		<div className='current-issues'>
-
+			<div>
+				<p>
+					Assigned <span>Username</span>
+				</p>
+				<p>
+					Priority <span>High</span>
+				</p>
+				<p>Description</p>
+			</div>
 		</div>
 	);
 };
